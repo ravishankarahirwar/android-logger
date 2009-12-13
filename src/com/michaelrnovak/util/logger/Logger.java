d3537ecd9c1c47ef4e8e1a1c56e69738b59789af
Mike Novak <mike@androidnerds.org>
2009-12-13 02:52:03 -0500

    adding license information

--- src/com/michaelrnovak/util/logger/Logger.java
+++ src/com/michaelrnovak/util/logger/Logger.java
@@ -1,17 +1,19 @@
 /*
- * Copyright (C) 2009 Michael Novak <mike@androidnerds.org>
+ * Copyright (C) 2009  Michael Novak <mike@androidnerds.org>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 package com.michaelrnovak.util.logger;
 
@@ -50,6 +52,7 @@ import android.widget.Toast;
 import com.michaelrnovak.util.logger.service.ILogProcessor;
 import com.michaelrnovak.util.logger.service.LogProcessor;
 
+import java.io.File;
 import java.util.HashMap;
 
 public class Logger extends Activity {
@@ -86,6 +89,13 @@ public class Logger extends Activity {
     public void onResume() {
     	super.onResume();
     	bindService(new Intent(this, LogProcessor.class), mConnection, Context.BIND_AUTO_CREATE);
+    	
+    	//little housekeeping.
+    	File f = new File("/sdcard/tmp.log");
+    	if (f.exists()) {
+    		f.delete();
+    	}
+    	
     }
     
     @Override
